15168b8d95de694d6c7e97009d327ceefd9f5d5b
Maxim <maxim.pishniy@gmail.com>
2025-06-09 11:59:36 +0500

    refactor: enhance proxy to work with different type of content

--- src/server.py
+++ src/server.py
@@ -1,37 +1,61 @@
+
 from flask import Flask, Response, request as flask_request
 from urllib.parse import urljoin
+
 import requests
 
-from handlers import WordLengthMarkerHandler
+from utils import filter_headers, encode_content, modify_response_html, modify_response_text
 
 
-app = Flask('ProxyApp')
+app = Flask('ProxyApp', static_url_path='/flask_static')
 
 
 proxy_host: str = 'en.wikipedia.org'
 BASE_URL = f'https://{proxy_host}/'
 
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def generic_proxy(path: str):
-    request_headers = dict(flask_request.headers.items())
-    request_headers['Host'] = proxy_host
+    request_headers = filter_headers(dict(flask_request.headers.items()))
+    target_url = urljoin(BASE_URL, path)
 
-    with requests.Session() as session:
-        response = session.request(
-            method=flask_request.method,
-            url=urljoin(BASE_URL, path),
-            headers=request_headers,
-            params=flask_request.args.to_dict(),
-            cookies=flask_request.cookies.to_dict(),
-            json=flask_request.get_json(silent=True) or {},
-            allow_redirects=False
-        )
+    request_headers['host'] = proxy_host
+    if 'origin' in request_headers:
+        request_headers['origin'] = proxy_host
 
-    modified_text = WordLengthMarkerHandler().handle(response.text)
+    if 'referer' in request_headers:
+        request_headers['referer'] = urljoin(BASE_URL, path)
+
+    try:
+        with requests.Session() as session:
+            response = session.request(
+                method=flask_request.method,
+                url=target_url,
+                headers=request_headers,
+                params=flask_request.args.to_dict(),
+                cookies=flask_request.cookies.to_dict(),
+                json=flask_request.get_json(silent=True) or {},
+                allow_redirects=False
+            )
+    except requests.RequestException as e:
+        return Response(f"Unable to retrieve content from {target_url}", status=503)
+
+    response_headers = filter_headers(response.headers)
+
+    response_content_type = response_headers.get('content-type', '')
+    content_encoding = response_headers.get('content-encoding', 'identity')
+
+    response_body = response.content
+    if 'text/html' in response_content_type:
+        response_body = modify_response_html(response.text)
+    elif 'text/plain' in response_content_type:
+        response_body = modify_response_text(response.text)
+
+    response_body = encode_content(response_body, content_encoding)
 
     return Response(
-        modified_text,
+        response_body,
         response.status_code,
-        response.headers
+        response_headers
     )
---
+++ src/utils.py
@@ -0,0 +1,53 @@
+from io import BytesIO
+from bs4 import BeautifulSoup
+import gzip
+import zlib
+from handlers import WordLengthMarkerHandler
+
+
+def encode_content(text, content_encoding, text_encoding='utf-8'):
+    if isinstance(text, str):
+        text = text.encode(text_encoding)
+    if content_encoding == 'identity':
+        data = text
+    elif content_encoding in ('gzip', 'x-gzip'):
+        io = BytesIO()
+        with gzip.GzipFile(fileobj=io, mode='wb') as f:
+            f.write(text)
+        data = io.getvalue()
+    elif content_encoding == 'deflate':
+        data = zlib.compress(text)
+    else:
+        raise Exception("Unknown Content-Encoding: %s" % content_encoding)
+    return data
+
+
+def modify_response_html(text: str) -> str:
+    modify_handler = WordLengthMarkerHandler()
+    soup = BeautifulSoup(text, 'html.parser')
+
+    if not soup.body:
+        return text
+
+    for node in soup.body.find_all(text=True):
+        if not any((
+            node.find_parent('script'),
+            node.find_parent('link'),
+        )):
+            node.replace_with(modify_handler.handle(node.text))
+
+    return str(soup)
+
+
+def modify_response_text(text: str) -> str:
+    modify_handler = WordLengthMarkerHandler()
+    return modify_handler.handle(text)
+
+
+def filter_headers(headers):
+    hop_by_hop_headers = ('connection', 'keep-alive', 'proxy-authenticate', 'proxy-authorization', 'te', 'trailers', 'transfer-encoding', 'upgrade')
+    return {
+        k.lower(): v 
+        for k, v in headers.items()
+        if k.lower() not in hop_by_hop_headers
+    }
